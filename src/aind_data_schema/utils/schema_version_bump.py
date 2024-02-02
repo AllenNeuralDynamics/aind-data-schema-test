ba3b4f8dfb4a2648d8b0a20cde3e05cd87d82bc1
Yosef Bedaso <yosef.bedaso@alleninstitute.org>
2024-02-02 14:25:57 -0800

    adds schema version bump script

--- src/aind_data_schema/utils/schema_version_bump.py
+++ src/aind_data_schema/utils/schema_version_bump.py
@@ -1,9 +1,10 @@
-"""Utility script to check if schemas are version bumped"""
+"""Utility script for bumping schema versions"""
 
 import argparse
 import json
 import os
 import sys
+import subprocess
 from pathlib import Path
 from typing import Optional
 
@@ -11,57 +12,42 @@ import dictdiffer
 import semver
 
 
-def compare_versions(new_ver: str, old_ver: Optional[str]) -> (bool, Optional[str]):
+def bump_version(old_ver: Optional[str]) -> str:
     """
-    Compares an old version with a new version.
+    Bumps an old version to a new version .
     Parameters
     ----------
-    new_ver : str
-      Like "0.2.0"
+
     old_ver : str
       Like "0.1.3"
 
     Returns
     -------
-    (bool, Union[str, None])
-      If the version is bumped correctly, returns (True, None). Otherwise,
-      returns (False, an error message)
+    str
 
     """
-
-    try:
-        new_v = semver.Version.parse(new_ver)
-    except (TypeError, ValueError):
-        return (False, f"Malformed version: {new_ver}")
 
     try:
         old_v = semver.Version.parse(old_ver)
     except (TypeError, ValueError):
-        return (True, None)
+        return f"Malformed version: {old_ver}"
 
-    major_bump = new_v == old_v.bump_major()
-    minor_bump = new_v == old_v.bump_minor()
-    patch_bump = new_v == old_v.bump_patch()
+    new_v = old_v.bump_patch()
+    new_ver = f"{new_v.major}.{new_v.minor}.{new_v.patch}"
 
-    if major_bump or minor_bump or patch_bump:
-        return (True, None)
-    else:
-        return (
-            False,
-            f"Version not bumped correctly. New Version: {new_ver}. Old Version: {old_ver}",
-        )
+    return new_ver
 
 
-def run_job(new_schema_folder: str, old_schema_folder: str) -> None:
+def run_job(new_schema_folder: str, old_schema_folder: str, core_schemas_folder: str) -> None:
     """
     Loops through files in folders and creates a list of common files.
     For each common file, if they are equal, nothing happens. If they are
-    different, the schema_version in the new file is checked against the old.
-    An AssertionError is raised if the version is not bumped correctly.
-    Parameters
+    different, the old schema version will be bumped and replaced with a
+    new version in the corresponding core model.
     ----------
     new_schema_folder : str
     old_schema_folder : str
+    core_schemas_folder: str
 
     Returns
     -------
@@ -78,29 +64,27 @@ def run_job(new_schema_folder: str, old_schema_folder: str) -> None:
     files_in_old_schema_folder = os.listdir(Path(old_schema_folder))
     files_in_new_schema_folder = os.listdir(Path(new_schema_folder))
     common_files: list = list(set(files_in_old_schema_folder).intersection(set(files_in_new_schema_folder)))
-    version_comparison_issues = []
     for file in common_files:
         old_model = open_json_file(Path(old_schema_folder) / file)
         new_model = open_json_file(Path(new_schema_folder) / file)
         diff = dictdiffer.diff(old_model, new_model)
         is_equal = len(list(diff)) == 0
         if not is_equal:
-            new_schema_version = new_model["properties"]["schema_version"]["const"]
             old_schema_version_dict = old_model["properties"].get("schema_version")
             old_schema_version = (
                 None
                 if old_schema_version_dict is None or not isinstance(old_schema_version_dict, dict)
                 else old_schema_version_dict.get("const")
             )
-            v_check = compare_versions(new_schema_version, old_schema_version)
-            if v_check[0] is False:
-                version_comparison_issues.append(f"{file} - {v_check[1]}")
-    old_schema_v = semver.Version.parse(old_schema_version)
-    new_schema_v = old_schema_v.bump_patch()
-    print("*****", new_schema_v, "*****")
-    if len(version_comparison_issues) > 0:
-        version_comparison_issues.sort()
-        # raise AssertionError(f"There were issues checking the schema versions: {version_comparison_issues}")
+            new_schema_version = bump_version(old_schema_version)
+            core_schema_file = Path(core_schemas_folder) / file.replace("_schema.json", ".py")
+            sed_command = f'sed -i \'/^schema_version:/s/"[^\"]*"/"{new_schema_version}"/g\' {core_schema_file}'
+            try:
+                subprocess.run(sed_command, shell=True, check=True)
+                print(f"Schema version in {file.replace('_schema.json', '')} have been bumped to {new_schema_version}")
+            except subprocess.CalledProcessError:
+                print("Error while bumping schema version. Please check the file structure or permissions.")
+
     return None
 
 
@@ -117,8 +101,14 @@ if __name__ == "__main__":
         "--new-schema-folder",
         required=True,
     )
+    parser.add_argument(
+        "-c",
+        "--core-schemas-folder",
+        required=True,
+    )
     folder_args = parser.parse_args(sys_args)
     run_job(
         new_schema_folder=folder_args.new_schema_folder,
         old_schema_folder=folder_args.old_schema_folder,
+        core_schemas_folder=folder_args.core_schemas_folder
     )
