16093237fc12c2f1dfec996faa5decc531123067
Yosef Bedaso <yosef.bedaso@alleninstitute.org>
2024-02-20 15:15:04 -0800

    modifiy schema

--- src/aind_data_schema/core/acquisition.py
+++ src/aind_data_schema/core/acquisition.py
@@ -78,11 +78,11 @@ class ProcessingSteps(AindModel):
 
 
 class Acquisition(AindCoreModel):
-    """Description of an imaging acquisition session [another test version bump]"""
+    """Description of an imaging acquisition session [ test version bump]"""
 
     _DESCRIBED_BY_URL = AindCoreModel._DESCRIBED_BY_BASE_URL.default + "aind_data_schema/core/acquisition.py"
     describedBy: str = Field(_DESCRIBED_BY_URL, json_schema_extra={"const": _DESCRIBED_BY_URL})
-    schema_version: Literal["0.6.4"] = Field("0.6.4")
+    schema_version: Literal["0.6.5"] = Field("0.6.5")
     protocol_id: List[str] = Field([], title="Protocol ID", description="DOI for protocols.io")
     experimenter_full_name: List[str] = Field(
         ...,
